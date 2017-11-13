1b7b38d0fc47b1ae883dfdc45a677632095c7184
Darby Vranek <lyra@Darbys-MacBook-Pro.local>
2017-11-12 16:28:40 -0800

    completed session words

--- django_intro/assignments/session_words/apps/words/views.py
+++ django_intro/assignments/session_words/apps/words/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-import time
+import datetime
 
 def index(request):
 	return render(request, 'words/index.html')
@@ -9,14 +9,15 @@ def add_word(request):
 	if not 'records' in request.session:
 		request.session['records'] = list()
 	style_class = request.POST['radio_color']
+	if 'big_font' in request.POST:
+		style_class = 'big_%s' % style_class
 	new_record = {
 		'word': request.POST['new_word'],
 		'style_class': style_class,
-		'timestamp': time.strftime('%I:%M:%S%p, %B %d %Y', time.localtime())
+		'timestamp': datetime.datetime.now().strftime('%I:%M:%S%p, %B %d %Y')
 	}
-	print request.session['records']
-	request.session['records'].append(new_record)
-	print request.session['records']
+	request.session['records'].insert(0, new_record)
+	request.session.modified = True
 	return redirect('/session_words')
 
 def clear_session(request):
